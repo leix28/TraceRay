bf3716da7412359d412a2de90d4ab5432a174d91
LazyLie <leopard.lie@gmail.com>
2015-05-10 01:00:56 +0800

    init with opencv

---
+++ TraceRay/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TraceRay
+//
+//  Created by LazyLie on 15/5/10.
+//  Copyright (c) 2015年 LeonardXu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
