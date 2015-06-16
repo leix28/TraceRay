52ea18301aa438b23b553662d8118dd2ac2861f5
LazyLie <leopard.lie@gmail.com>
2015-06-16 18:47:47 +0800

    define 3D vector

---
+++ TraceRay/foundation.cpp
@@ -0,0 +1,74 @@
+//
+//  foundation.cpp
+//  TraceRay
+//
+//  Created by LazyLie on 15/6/16.
+//  Copyright (c) 2015年 LeonardXu. All rights reserved.
+//
+
+#include "foundation.h"
+
+Vector::Vector() {
+  x = y = z = 0;
+}
+
+double& Vector::operator [] (const int id) {
+  if (id == 0) return x;
+  if (id == 1) return y;
+  if (id == 2) return z;
+  assert(0);
+}
+
+double Vector::operator [] (const int id) const {
+  if (id == 0) return x;
+  if (id == 1) return y;
+  if (id == 2) return z;
+  assert(0);
+}
+
+Vector operator + (const Vector &a, const Vector &b) {
+  Vector c;
+  for (int i = 0; i < 3; i++) c[i] = a[i] + b[i];
+  return c;
+}
+
+Vector operator - (const Vector &a, const Vector &b) {
+  Vector c;
+  for (int i = 0; i < 3; i++) c[i] = a[i] - b[i];
+  return c;
+}
+
+Vector operator * (const double a, const Vector &b) {
+  Vector c;
+  for (int i = 0; i < 3; i++) c[i] = a * b[i];
+  return c;
+}
+
+Vector operator * (const Vector &a, const double b) {
+  return b * a;
+}
+
+Vector operator * (const Vector &a, const Vector &b) {
+  Vector c;
+  for (int i = 0; i < 3; i++) c[i] = a[i] * b[i];
+  return c;
+}
+
+Vector operator / (const Vector &a, const double b) {
+  assert(fabs(b) >= EPS);
+  return 1 / b * a;
+}
+
+double innerProduct(const Vector &a, const Vector &b) {
+  double c = 0;
+  for (int i = 0; i < 3; i++) c += a[i] * b[i];
+  return c;
+}
+
+Vector crossProduct(const Vector &a, const Vector &b) {
+  Vector c;
+  c[0] = a[1] * b[2] - a[2] * b[1];
+  c[1] = a[2] * b[0] - a[0] * b[2];
+  c[2] = a[0] * b[1] - a[1] * b[0];
+  return c;
+}
---
+++ TraceRay/foundation.h
@@ -0,0 +1,44 @@
+//
+//  foundation.h
+//  TraceRay
+//
+//  Created by LazyLie on 15/6/16.
+//  Copyright (c) 2015年 LeonardXu. All rights reserved.
+//
+
+#ifndef __TraceRay__foundation__
+#define __TraceRay__foundation__
+
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#define EPS 1e-8
+
+class Vector {
+public:
+  Vector();
+  Vector(double _x, double _y, double _z);
+  double& operator [] (const int id);
+  double operator [] (const int id) const;
+  
+private:
+  double x, y, z;
+};
+
+Vector operator + (const Vector &a, const Vector &b);
+Vector operator - (const Vector &a, const Vector &b);
+Vector operator * (const double a, const Vector &b);
+Vector operator * (const Vector &a, const double b);
+Vector operator * (const Vector &a, const Vector &b);
+Vector operator / (const Vector &a, const double b);
+double innerProduct(const Vector &a, const Vector &b);
+Vector crossProduct(const Vector &a, const Vector &b);
+
+#endif /* defined(__TraceRay__foundation__) */
