c272d8378723523d2f3f823e7d3543c1a64eedf5
LazyLie <leopard.lie@gmail.com>
2015-06-21 20:37:55 +0800

    get back camera

---
+++ TraceRay/camera.cpp
@@ -0,0 +1,44 @@
+//
+//  camera.cpp
+//  TraceRay
+//
+//  Created by LazyLie on 15/6/21.
+//  Copyright (c) 2015年 LeonardXu. All rights reserved.
+//
+
+#include "camera.h"
+
+Vector Camera::getFilm(double row, double col) {
+  return filmCenter + filmHeight * (row - 0.5) + filmWidth * (col - 0.5);
+}
+
+std::vector<Ray> Camera::getRay(int row, int col) {
+  Ray r;
+  std::vector<Ray> rv;
+  Vector target, d, p, n;
+  p = getFilm((double)row / pixHeight, (double)col / pixWidth);
+  d = p - position;
+  d = d / norm(d);
+  if (sample == 0) {
+    r.position = p;
+    r.direction = d;
+    rv.push_back(r);
+    return rv;
+  }
+  
+  n = crossProduct(filmWidth, filmHeight);
+  n = n / norm(n);
+  
+  
+  target = p + d * dis / innerProduct(d, n);
+  for (int i = -sample; i <= sample; i++)
+    for (int j = -sample; j <= sample; j++) {
+      r.position = getFilm((double)row / pixHeight + aperture * i / sample, (double)col / pixWidth + aperture * j / sample);
+      r.direction = target - r.position;
+      assert(norm(r.direction) > EPS);
+      r.direction = r.direction / norm(r.direction);
+      r.inside = 0;
+      rv.push_back(r);
+    }
+  return rv;
+}
---
+++ TraceRay/camera.h
@@ -0,0 +1,26 @@
+//
+//  camera.h
+//  TraceRay
+//
+//  Created by LazyLie on 15/6/21.
+//  Copyright (c) 2015年 LeonardXu. All rights reserved.
+//
+
+#ifndef __TraceRay__camera__
+#define __TraceRay__camera__
+
+#include <stdio.h>
+#include "core.h"
+
+class Camera {
+public:
+  Vector filmCenter, filmHeight, filmWidth, position;
+  int pixHeight, pixWidth;
+  int sample;
+  double aperture;
+  double dis;
+  Vector getFilm(double row, double col);
+  std::vector<Ray> getRay(int row, int col);
+};
+
+#endif /* defined(__TraceRay__camera__) */
