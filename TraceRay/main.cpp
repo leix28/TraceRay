1cabede9c95660c19a807fb26a9703472744d630
LazyLie <leopard.lie@gmail.com>
2015-07-02 01:11:28 +0800

    aperture

--- TraceRay/main.cpp
+++ TraceRay/main.cpp
@@ -12,7 +12,7 @@
 Scene scene;
 
 int main(int argc, const char * argv[]) {
-  scene.MC_NUM = 100;
+  scene.MC_NUM = 50;
 
   scene.camera.position = Vector(5, 5, 20);
   scene.camera.filmCenter = Vector(5, 5, 10);
@@ -20,8 +20,8 @@ int main(int argc, const char * argv[]) {
   scene.camera.pixWidth = 1000;
   scene.camera.filmHeight = Vector(0, -10, 0);
   scene.camera.filmWidth = Vector(10, 0, 0);
-  scene.camera.aperture = 0.001;
-  scene.camera.sample = 10;
+  scene.camera.aperture = 0.008;
+  scene.camera.sample = 20;
   scene.camera.dis = 4;
 
   scene.MAX_DEP = 5;
@@ -83,18 +83,36 @@ int main(int argc, const char * argv[]) {
   scene.item.push_back(make_pair(RECT, rect));
 
 
-//  Obj *obj = new Obj();
-//  obj->load("bunny.obj", Vector(0.15, -0.0333, 0.15), 40);
-//  obj->attribute.kd = Vector(0.8, 0.8, 0.5);
-//  obj->attribute.ka = Vector(0.2, 0.2, 0.2);
-//  obj->attribute.index = 1.7;
-//  scene.item.push_back(make_pair(OBJ, obj));
+  Ball *ball = new Ball();
+  ball->position = Vector(7, 7.5, 7);
+  ball->radius = 2;
+  ball->attribute.ks = Vector(0, 0, 0);
+  ball->attribute.kd = Vector(0, 0, 0);
+  ball->attribute.ka = Vector(0, 0, 0);
+  ball->attribute.kt = Vector(1, 1, 1);
+  ball->attribute.index = 1.7;
+  scene.item.push_back(make_pair(BALL, ball));
 
-  Obj *obj = new Obj();
-  obj->load("kitten.obj", Vector(51.21, 3.16, 60.80), 0.1);
-  obj->attribute.kt = Vector(1, 0.8, 0.5);
-  obj->attribute.index = 1.7;
-  scene.item.push_back(make_pair(OBJ, obj));
+  ball = new Ball();
+  ball->position = Vector(3, 5.5, 4);
+  ball->radius = 1.5;
+  ball->attribute.ks = Vector(0, 0, 0);
+  ball->attribute.kd = Vector(1, 1, 1);
+  ball->attribute.ka = Vector(0, 0, 0);
+  ball->attribute.kt = Vector(0, 0, 0);
+  ball->attribute.index = 1.7;
+  ball->attribute.loadImg("maoxian.png");
+  scene.item.push_back(make_pair(BALL, ball));
+
+
+  Attribute attr;
+  attr.ks = Vector(0.2, 0.2, 0.2);
+  attr.kd = Vector(1, 1, 1);
+  attr.ka = Vector(0, 0, 0);
+  attr.kt = Vector(0, 0, 0);
+  attr.loadImg("marble.png");
+  insertBox(scene.item, Vector(1, 0, 4), Vector(2, 0, -2), Vector(0, 4, 0), Vector(2, 0, 2), attr);
+  insertBox(scene.item, Vector(7, 0, 6), Vector(2, 0, 1), Vector(0, 3, 0), Vector(-1, 0, 2), attr);
 
   Rect lit;
   lit.position = Vector(3.5, 9.999, 3.5);
@@ -107,8 +125,12 @@ int main(int argc, const char * argv[]) {
   scene.light = lit;
   scene.lightSample = 1;
 
-  scene.render();
-  scene.save("out.png");
-
+  for (int i = 3; i < 8; i += 2) {
+    scene.camera.dis = i;
+    scene.render();
+    std::string name = "0.png";
+    name[0] = '0' + i;
+    scene.save(name);
+  }
   return 0;
 }
